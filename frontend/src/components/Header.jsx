df53013f543d9491e5de599f7e8dda4131fa5d41
arnav <arnav.bholausa@gmail.com>
2024-11-10 07:12:44 +0000

    design changes

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <div>
+        <h1>adf</h1>
+    </div>
+}
+
+export default Header;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        "500": "#1A245B"
+      },
+    },
   },
   plugins: [],
 }
