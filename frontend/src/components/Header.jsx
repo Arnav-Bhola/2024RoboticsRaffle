d4e434addfda490308fab66bcd8cfc4825becbe7
arnav <arnav.bholausa@gmail.com>
2024-11-10 16:04:52 -0500

    creating interface

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,38 @@
 const Header = () => {
   return (
-    <div className='flex items-center justify-center'>
-      <h1 className='font-stencil text-9xl font-outline-2 text-blue-500 tracking-tight'>
+    <div className='flex items-center justify-center h-[91.5vh] flex-col mr-[15rem] ml-[15rem] overflow-hidden'>
+      <h1 className='font-stencil text-9xl font-outline-2 text-blue-500 tracking-tight text-shadow'>
         ENFORCERS
       </h1>
+      <div className='flex flex-col justify-center items-center mt-4'>
+        <h2 className='text-blue-500 font-bold text-4xl font-opensans'>Robotics Raffle</h2>
+        <h2 className='text-blue-500 font-bold text-4xl font-opensans'>2024&#45;2025</h2>
+      </div>
+
+      <div className='relative w-full rounded-xl pt-[0.1rem] pb-[0.1rem] bg-[#d9d9d9] mt-8'>
+        a{" "}
+        <div className='absolute left-0 top-0 h-full items-center progress w-[39%] flex justify-end pr-5 rounded-l-xl text-white-100'>
+          <h1 className='z-10 font-opensans'>39%</h1>
+        </div>
+      </div>
+
+      <div className='flex justify-between w-full mt-14 font-opensans'>
+        <div className='flex flex-col w-[15rem] text-blue-500 items-center font-semibold text-2xl border-4 border-blue-500 rounded-xl pr-8 pl-8 pt-2 pb-2 text-nowrap'>
+          <h3 className='text-3xl'>1401</h3>
+          <h4>Tickets Sold</h4>
+        </div>
+        <div className='flex flex-col w-[15rem] text-blue-500 items-center font-semibold text-2xl border-4 border-blue-500 rounded-xl pr-8 pl-8 pt-2 pb-2 text-nowrap'>
+          <h3 className='text-3xl'>$6000</h3>
+          <h4>Sales Raised</h4>
+        </div>
+        <div className='flex flex-col w-[15rem] text-blue-500 items-center font-semibold text-2xl border-4 border-blue-500 rounded-xl pr-8 pl-8 pt-2 pb-2 text-nowrap'>
+          <h3 className='text-3xl'>1000 Hrs</h3>
+          <h4>Until Prize Pull</h4>
+        </div>
+      </div>
+      <a className='bg-yellow-400 rounded-lg flex items-center justify-center text-center pr-12 pl-12 pt-2 pb-2 text-3xl font-bold font-opensans mt-10'>
+        Buy a ticket!
+      </a>
     </div>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,19 @@ import logo from "../assets/images/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-evenly items-center pb-[0.75rem] pt-[0.75rem] pr-[13rem] pl-[13rem] h-[8.5vh] bg-blue-500 relative'>
-      <div className='w-[50%] h-full flex justify-start  text-start justify-self-start brightness-100 hover:brightness-150 cursor-pointer'>
-        <img
-          src={logo}
-          className='h-full w-fit'
-        />
+    <div className='flex xl:justify-evenly md:justify-between justify-center gap-16 items-center pb-[0.75rem] pt-[0.75rem] xl:pr-[13rem] pr-[8rem] xl:pl-[13rem] pl-[8rem] h-[8.5vh] bg-blue-500 relative shadow-2xl'>
+      <div className='min-w-[20%] h-full flex justify-start  text-start justify-self-start brightness-100 hover:brightness-150 cursor-pointer'>
+        <a
+          className='h-full w-fit justify-self-start'
+          href='https://farmingtonrobotics.org/'
+        >
+          <img
+            src={logo}
+            className='h-full w-fit justify-self-start'
+          />
+        </a>
       </div>
-      <div className='w-full flex items-center justify-evenly text-white-200 text-[1.35rem] font-opensans'>
+      <div className='w-full items-center xl:justify-evenly justify-self-center text-white-200 text-[1.3rem] font-opensan0 no-wrap hidden xl:flex'>
         <a
           href='https://farmingtonrobotics.org/about'
           className='hover:-translate-y-[0.1rem] transition-all duration-100'
@@ -35,7 +40,7 @@ const Navbar = () => {
           Contact Us
         </a>
       </div>
-      <div className='flex h-fit w-[50%] items-center justify-end'>
+      <div className='h-fit lg:w-[30%] w-fit items-center justify-end hidden md:flex'>
         <a
           className='inline-block align-top mt-0 mb-0 mr-[5px] ml-[5px] w-[32px] h-[32px] bg-cover hover:scale-[1.1] transition-all duration-200'
           id='twitter-link'
